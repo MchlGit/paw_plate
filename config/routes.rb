6c69427c1f74cd269f5bf9786df135ef2d29a69f
MchlGit <60210564+MchlGit@users.noreply.github.com>
2025-10-21 21:15:04 -0700

    Added in user controller via devise and now can login.

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  get "home/index"
+  root "home#index"
 end
